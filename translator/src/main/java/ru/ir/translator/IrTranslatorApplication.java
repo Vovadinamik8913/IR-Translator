110d2e6f6a865ee6cce12e6220c5c6ad6d664388
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-12-25 18:21:22 +0700

    adding workpath

--- translator/src/main/java/ru/ir/translator/IrTranslatorApplication.java
+++ translator/src/main/java/ru/ir/translator/IrTranslatorApplication.java
@@ -3,11 +3,19 @@ package ru.ir.translator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class IrTranslatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IrTranslatorApplication.class, args);
+
+        try {
+            LocalConfig.deserializeFromJson();
+			SpringApplication.run(IrTranslatorApplication.class, args);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 	}
 
 }
