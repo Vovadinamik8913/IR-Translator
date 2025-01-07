69a81b9fa5bd5b9bf0950246f25dc429982e6001
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2025-01-07 14:51:46 +0700

    lang controller

---
+++ translator/src/main/java/ru/ir/translator/controller/LangController.java
@@ -0,0 +1,91 @@
+package ru.ir.translator.controller;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.ir.translator.model.classes.lang.*;
+import ru.ir.translator.view.service.LangService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/lang")
+public class LangController {
+
+    private final LangService langService;
+
+    @Operation(summary = "Получение языков")
+    @PostMapping(value = "/languages")
+    public ResponseEntity<List<String>> getLanguages() {
+        List<Language> languages = langService.getLanguages();
+        List<String> result = new ArrayList<>();
+        for (Language language : languages) {
+            result.add(language.getLang().getName());
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(summary = "Получение компилятора")
+    @PostMapping(value = "/compilers")
+    public ResponseEntity<List<String>> getCompilers(
+            @Parameter(description = "Language", required = true) @RequestParam("lang") String lang
+    ) {
+        Language language = langService.getLanguage(lang);
+        System.out.println(language.getLang().getName());
+        List<Compiler> compilers = langService.getCompilers(language);
+        List<String> result = new ArrayList<>();
+        for (Compiler compiler : compilers) {
+            result.add(compiler.getName());
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(summary = "Получение представлений")
+    @PostMapping(value = "/representation")
+    public ResponseEntity<List<String>> getRepresentations(
+            @Parameter(description = "Language", required = true) @RequestParam("compiler") String compiler
+    ) {
+        Compiler comp = langService.getCompiler(compiler);
+        List<LLLanguage> languages = langService.getLLLanguages(comp);
+        if (languages == null || languages.isEmpty()) {
+            return ResponseEntity.ok(new ArrayList<>());
+        }
+        List<String> result = new ArrayList<>();
+        for (LLLanguage language : languages) {
+            result.add(language.getType().getName());
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(summary = "Получение расширения")
+    @PostMapping(value = "/code-extension")
+    public ResponseEntity<String> getCodeExtension(
+            @Parameter(description = "Language", required = true) @RequestParam("lang") String lang
+    ) {
+        Lang type = Lang.of(lang);
+        if (type == null) {
+            return ResponseEntity.ofNullable(null);
+        }
+        return ResponseEntity.ok(type.getExtension());
+    }
+
+    @Operation(summary = "Получение расширения")
+    @PostMapping(value = "/repr-extension")
+    public ResponseEntity<String> getReprExtension(
+            @Parameter(description = "Language", required = true) @RequestParam("lang") String lang
+    ) {
+        LLLang type = LLLang.of(lang);
+        if (type == null) {
+            return ResponseEntity.ofNullable(null);
+        }
+        return ResponseEntity.ok(type.getExtension());
+    }
+}
