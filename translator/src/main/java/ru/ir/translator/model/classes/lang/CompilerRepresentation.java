7b257e4c3979efc066f7a7a3ab756f90f6065591
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2025-01-07 15:49:09 +0700

    small changes

--- translator/src/main/java/ru/ir/translator/model/classes/lang/CompilerRepresentation.java
+++ translator/src/main/java/ru/ir/translator/model/classes/lang/CompilerRepresentation.java
@@ -21,6 +21,24 @@ public class CompilerRepresentation {
     @JoinColumn(name = "compiler_id")
     private Compiler compiler;
 
+    @Transient
+    private String[] flags;
+
+    @Basic
     private String specialFlags;
+
+    @PostLoad
+    private void fillTransient() {
+        if (specialFlags != null) {
+            this.flags = specialFlags.split(" ");
+        }
+    }
+
+    @PrePersist
+    private void fillPersistent() {
+        if (flags != null) {
+            this.specialFlags = String.join(" ", flags);
+        }
+    }
 }
 
--- translator/src/main/java/ru/ir/translator/model/classes/lang/Lang.java
+++ translator/src/main/java/ru/ir/translator/model/classes/lang/Lang.java
@@ -6,20 +6,18 @@ import java.util.stream.Stream;
 
 @Getter
 public enum Lang {
-    C(100, "c", "c"),
-    CPP(200, "cpp", "cpp"),
-    RUST(300, "rs", "rust"),
-    SWIFT(400, "swift", "swift"),
-    HASKELL(500, "hs", "haskell"),
-    D(600, "d", "d"),
-    JAVA(700, "java", "java"),
-    LLVMIR(800, "ll", "LLVM IR");
+    C("c", "c"),
+    CPP("cpp", "cpp"),
+    RUST("rs", "rust"),
+    SWIFT("swift", "swift"),
+    HASKELL("hs", "haskell"),
+    D("d", "d"),
+    JAVA("java", "java"),
+    LLVMIR("ll", "LLVM IR");
 
-    private final int code;
     private final String extension;
     private final String name;
-    Lang(int i, String s, String s1) {
-        code = i;
+    Lang(String s, String s1) {
         extension = s;
         name = s1;
     }
@@ -27,14 +25,6 @@ public enum Lang {
     public static Lang of(String name) throws IllegalArgumentException {
         return Stream.of(Lang.values())
                 .filter(l -> l.name.equals(name))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
-    }
-
-
-    public static Lang of(int code) throws IllegalArgumentException {
-        return Stream.of(Lang.values())
-                .filter(l -> l.code  == code)
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
     }
