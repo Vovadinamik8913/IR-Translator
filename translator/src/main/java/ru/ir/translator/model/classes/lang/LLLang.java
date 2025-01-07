8add395c64ce3ad5f55850ca649145bb73c18bd3
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2025-01-07 15:42:32 +0700

    small changes

--- translator/src/main/java/ru/ir/translator/model/classes/lang/LLLang.java
+++ translator/src/main/java/ru/ir/translator/model/classes/lang/LLLang.java
@@ -6,24 +6,15 @@ import java.util.stream.Stream;
 
 @Getter
 public enum LLLang {
-    ASM(100, "s", "ASM"),
-    IR(200, "ll", "LLVM IR")
-    JBC(300, "class", "JBC");
+    ASM("s", "ASM"),
+    IR("ll", "LLVM IR"),
+    JBC("class", "JBC");
 
-    private final int code;
     private final String extension;
     private final String name;
-    LLLang(int i, String s, String assembler) {
-        code = i;
+    LLLang(String s, String assembler) {
         extension = s;
         name = assembler;
-    }
-
-    public static LLLang of(int code) throws IllegalArgumentException {
-        return Stream.of(LLLang.values())
-                .filter(l -> l.code  == code)
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
     }
 
     public static LLLang of(String name) throws IllegalArgumentException {
